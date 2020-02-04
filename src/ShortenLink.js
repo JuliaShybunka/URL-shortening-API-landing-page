fb5ed5e16ec9ed06d81b085845715858213fe6c9
JuliaShybunka <julishybunka@gmail.com>
2020-02-04 10:57:45 +0200

    Add shorten link style

--- src/ShortenLink.js
+++ src/ShortenLink.js
@@ -5,9 +5,13 @@ const ShortenLink = () => {
     <div className="shorten-link">
       <div className="container">
         <div className="shorten-link__form">
-          <form>
-            <input type="text" />
-            <button>Shorten it!</button>
+          <form className="shorten-link__form-link">
+            <div>
+              <input type="text" placeholder="Shorten a link here..." />
+              <button className="shorten-link__form-link-btn">
+                Shorten it!
+              </button>
+            </div>
           </form>
         </div>
         <div className="shorten-link__list"></div>
