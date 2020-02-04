20acf063f0e03a8f201281545507f93e65fbe0e2
JuliaShybunka <julishybunka@gmail.com>
2020-02-04 16:54:10 +0200

    Update shorten link component

--- src/ShortenLink.js
+++ src/ShortenLink.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ShortenLink = () => {
+  const [shortenLink, setShortenLink] = useState([]);
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
   return (
     <div className="shorten-link">
       <div className="container">
         <div className="shorten-link__form">
-          <form className="shorten-link__form-link">
+          <form className="shorten-link__form-link" onSubmit={handleSubmit}>
             <div>
-              <input type="text" placeholder="Shorten a link here..." />
-              <button className="shorten-link__form-link-btn">
+              <input
+                onChange={event => {
+                  setShortenLink(event.target.value);
+                }}
+                type="text"
+                placeholder="Shorten a link here..."
+              />
+              <button type="submit" className="shorten-link__form-link-btn">
                 Shorten it!
               </button>
+
+              <p></p>
             </div>
           </form>
         </div>
