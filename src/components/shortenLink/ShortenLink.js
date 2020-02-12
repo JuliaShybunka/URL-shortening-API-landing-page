406e69aa8903116e0baf53d9d19d0504a2d538f8
JuliaShybunka <julishybunka@gmail.com>
2020-02-12 16:37:29 +0200

    Change ShortenLink component (as class component). Add shorten link button

--- src/components/shortenLink/ShortenLink.js
+++ src/components/shortenLink/ShortenLink.js
@@ -1,35 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
 
-const ShortenLink = () => {
-  const [shortenLink, setShortenLink] = useState([]);
-  const handleSubmit = event => {
-    event.preventDefault();
+class ShortenLink extends React.Component {
+  state = {
+    linkText: "",
+    li: []
   };
 
-  return (
-    <div className="shorten-link">
-      <div className="container">
-        <div className="shorten-link__form">
-          <form className="shorten-link__form-link" onSubmit={handleSubmit}>
-            <div>
-              <input
-                onChange={event => {
-                  setShortenLink(event.target.value);
-                }}
-                type="text"
-                placeholder="Shorten a link here..."
-              />
-              <button type="submit" className="shorten-link__form-link-btn">
-                Shorten it!
-              </button>
-              <p>{shortenLink}</p>
-            </div>
-          </form>
+  handleChange = event => {
+    this.setState({ linkText: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      li: [...this.state.li, this.state.linkText]
+    });
+  };
+  render() {
+    const { li } = this.state;
+    return (
+      <div className="shorten-link">
+        <div className="container">
+          <div className="shorten-link__form">
+            <form
+              className="shorten-link__form-link"
+              onSubmit={this.handleSubmit}
+            >
+              <div>
+                <input
+                  onChange={this.handleChange}
+                  type="text"
+                  placeholder="Shorten a link here..."
+                />
+                <button type="submit" className="shorten-link__form-link-btn">
+                  Shorten it!
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="shorten-link__list">
+            <p>
+              <ul>
+                {li.map(e => (
+                  <li>{e}</li>
+                ))}
+              </ul>
+            </p>
+          </div>
         </div>
-        <div className="shorten-link__list"></div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ShortenLink;
