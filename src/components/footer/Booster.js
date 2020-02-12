30d02a37334faa0e97be1b7fcd451adbda3036ac
JuliaShybunka <julishybunka@gmail.com>
2020-02-12 22:11:06 +0200

    Add Booster style

--- src/components/footer/Booster.js
+++ src/components/footer/Booster.js
@@ -3,11 +3,9 @@ import React from "react";
 const Booster = () => {
   return (
     <div className="booster">
-      <div className="container">
+      <div className="container booster__inner">
         <div className="booster__name">Boost your links today</div>
-        <a href="/" className="booster__btn">
-          Get Started
-        </a>
+        <button className="booster__btn">Get Started</button>
       </div>
     </div>
   );
