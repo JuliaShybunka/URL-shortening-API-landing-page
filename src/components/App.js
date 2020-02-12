5830b2ce17d0f0cc8f53043877c3c6c7b847f271
JuliaShybunka <julishybunka@gmail.com>
2020-02-12 21:31:21 +0200

    Create Booster component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./header/Header";
 import HeaderBanner from "./header/HeaderBanner";
 import ShortenLink from "./shortenLink/ShortenLink";
 import About from "./about/About";
+import Booster from "./footer/Booster";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HeaderBanner />
       <ShortenLink />
       <About />
+      <Booster />
     </>
   );
 }
---
+++ src/components/footer/Booster.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Booster = () => {
+  return (
+    <div className="booster">
+      <div className="container">
+        <div className="booster__name">Boost your links today</div>
+        <a href="/" className="booster__btn">
+          Get Started
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Booster;
